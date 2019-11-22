60349921ebc89ef7e7722dfd5d9bf0f4f4b08160
nrico90 <nrico90@gmail.com>
2019-11-22 15:52:43 +0100

    change about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PagesForm from "./PagesForm";
 import { Link } from "react-router-dom";
 import "./AboutUs.css";
 
@@ -16,6 +17,8 @@ export default function AboutUs(props) {
       </b>
       <img src="https://scontent.fams1-2.fna.fbcdn.net/v/t1.0-9/41921278_2037197352977596_3531365829243305984_n.jpg?_nc_cat=104&_nc_oc=AQlaFpst0mYMp-IPncHiCL1b4ue2DBgVKrtmAJUM3H8Ocy2shAMyVe7gnUAChtVyk0U&_nc_ht=scontent.fams1-2.fna&oh=8c6ded1e9c7b6b64e068e8f19d71db2a&oe=5E82A25A"></img>
       <img src="https://scontent.fams1-1.fna.fbcdn.net/v/t1.0-9/30441498_1492311624210683_3388239041991278592_n.jpg?_nc_cat=111&_nc_oc=AQkCRSqC88-3pdUkohlT-YzdgjYF_uXLlsRW-J_BHAGrN6eay13_3Rthx7iaQOQzwrg&_nc_ht=scontent.fams1-1.fna&oh=55f10b4c8f3f3e5efeceee54ac74c403&oe=5E8343D5"></img>
+
+      <PagesForm />
     </div>
   );
 }
